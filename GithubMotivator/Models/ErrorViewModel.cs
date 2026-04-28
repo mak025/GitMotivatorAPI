ef2471ffda19fa098f2d4cea5d8de8b4e0ad31f8
mak025 <mak025@edu.zealand.dk>
2026-04-28 10:05:26 +0200

    Initial commit

---
+++ GithubMotivator/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GithubMotivator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
