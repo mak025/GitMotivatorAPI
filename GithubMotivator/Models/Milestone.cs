18a7bda4014dfde29e98dc011114b45a76516bc8
Egil Bukhave Edström <ege001@edu.zealand.dk>
2026-05-11 10:34:03 +0200

    Milestone class created

---
+++ GithubMotivator/Models/Milestone.cs
@@ -0,0 +1,9 @@
+﻿namespace GithubMotivator.Models
+{
+    public class Milestone
+    {
+        public int Id { get; set; }
+        public int CommitTreshold { get; set; }
+        public string Message { get; set; } = string.Empty;
+    }
+}
